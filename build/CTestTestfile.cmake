6ca4b74d9548a12a147ab84edd7d9ddf1670b9c6
hanyuxuan <yasin.hyx0811@gmail.com>
2024-01-08 12:32:12 +0800

    remove build gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/han/Code/ray_trace
-# Build directory: /home/han/Code/ray_trace/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/han/Code/ray_trace/build/tests[1]_include.cmake")
-subdirs("_deps/googletest-build")
--- build/tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/han/Code/ray_trace/build/tests[1]_tests.cmake")
-  include("/home/han/Code/ray_trace/build/tests[1]_tests.cmake")
-else()
-  add_test(tests_NOT_BUILT tests_NOT_BUILT)
-endif()
