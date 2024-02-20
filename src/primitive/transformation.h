2fbc6deb5a10ae1c20379ac2bf026c3859672606
yasin.han <yasin.hyx0811@gmail.com>
2024-02-20 21:22:33 +0800

    transform

---
+++ src/primitive/transformation.h
@@ -0,0 +1,13 @@
+//
+// Created by han on 1/8/24.
+//
+
+#ifndef RAY_TRACE_TRANSFORMATION_H
+#define RAY_TRACE_TRANSFORMATION_H
+
+#include "matrix.h"
+
+Matrix translation(float x, float y, float z);
+
+
+#endif //RAY_TRACE_TRANSFORMATION_H
