c5ffb805889d02d723c244dfdc59633367ba4c5e
hanyuxuan <yasin.hyx0811@gmail.com>
2024-04-07 17:23:16 +0800

    remove ptr in test

--- src/test/display/canvas_test.cpp
+++ src/test/display/canvas_test.cpp
@@ -15,14 +15,14 @@ protected:
 };
 
 TEST(CANVAS_TEST, TEST_CREATE) {
-    auto canvas = new Canvas(3, 3);
-    auto color_0_0 = canvas->pixel_at(0, 0);
+    auto canvas = Canvas(3, 3);
+    auto color_0_0 = canvas.pixel_at(0, 0);
 
     EXPECT_TRUE(epsilon(color_0_0.get_red(), 0.0));
     EXPECT_TRUE(epsilon(color_0_0.get_green(), 0.0));
     EXPECT_TRUE(epsilon(color_0_0.get_blue(), 0.0));
 
-    auto color_0_1 = canvas->pixel_at(0, 1);
+    auto color_0_1 = canvas.pixel_at(0, 1);
 
     EXPECT_TRUE(epsilon(color_0_1.get_red(), 0.0));
     EXPECT_TRUE(epsilon(color_0_1.get_green(), 0.0));
@@ -30,11 +30,11 @@ TEST(CANVAS_TEST, TEST_CREATE) {
 }
 
 TEST(CANVAS_TEST, TEST_WRITE) {
-    auto canvas = new Canvas(3, 3);
-    auto color = new Color(1, 0.1, 0.5);
-    canvas->write_pixel(0, 1, *color);
+    auto canvas = Canvas(3, 3);
+    auto color = Color(1, 0.1, 0.5);
+    canvas.write_pixel(0, 1, color);
 
-    auto color_in = canvas->pixel_at(0, 1);
+    auto color_in = canvas.pixel_at(0, 1);
 
     EXPECT_TRUE(epsilon(color_in.get_red(), 1));
     EXPECT_TRUE(epsilon(color_in.get_green(), 0.1));
