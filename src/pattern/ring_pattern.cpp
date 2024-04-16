27f57976425f8a9ce2bdfd27397f58dc6150d3a6
hanyuxuan <yasin.hyx0811@gmail.com>
2024-04-16 15:34:23 +0800

    finish ring pattern

--- src/pattern/ring_pattern.cpp
+++ src/pattern/ring_pattern.cpp
@@ -3,9 +3,12 @@
 //
 
 #include "ring_pattern.h"
+#include <cmath>
 
 RingPattern::RingPattern(const Color &a, const Color &b) : a_(a), b_(b) {}
 
 Color RingPattern::color_at_pattern(const Tuple &point) const {
-    return Color();
+    auto distance = std::sqrt(std::pow(point.getX(), 2) + std::pow(point.getZ(), 2));
+    return (int )std::floor(distance) % 2 == 0 ? a_ : b_;;
+
 }
